79e898fc87b1032703e4074cb87ed884463ce82c
vauxhall <zero.griffin@gmail.com>
2018-11-23 14:23:04 +0100

    Create item view

---
+++ DesktopUI/ItemView.swift
@@ -0,0 +1,42 @@
+import Cocoa
+
+class ItemView:NSControl {
+    private weak var field:NSTextField!
+    override var intrinsicContentSize:NSSize { return NSSize(width:120, height:60) }
+    var selected = false { didSet {
+        if selected {
+            layer!.backgroundColor = NSColor.selectedMenuItemColor.cgColor
+        } else {
+            layer!.backgroundColor = NSColor.clear.cgColor
+        }
+    } }
+    
+    override func mouseDown(with:NSEvent) {
+        if !selected {
+            sendAction(action, to:target)
+        }
+    }
+    
+    init() {
+        super.init(frame:.zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        wantsLayer = true
+        
+        let field = NSTextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.font = .systemFont(ofSize:14, weight:.medium)
+        field.backgroundColor = .clear
+        field.lineBreakMode = .byTruncatingTail
+        field.isBezeled = false
+        field.isEditable = false
+        field.stringValue = "Hello world, lorem ipsum, dolet samont"
+        addSubview(field)
+        self.field = field
+        
+        field.leftAnchor.constraint(equalTo:leftAnchor, constant:10).isActive = true
+        field.rightAnchor.constraint(equalTo:rightAnchor, constant:-10).isActive = true
+        field.topAnchor.constraint(equalTo:topAnchor, constant:10).isActive = true
+    }
+    
+    required init?(coder:NSCoder) { return nil }
+}
---
+++ DesktopUI/Presenter.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class Presenter {
+    var updateItems:(([String]) -> Void)!
+    
+    
+}
