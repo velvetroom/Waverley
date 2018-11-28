1cf3ddb295971cf74bf34c4e0437264a97708e91
vauxhall <zero.griffin@gmail.com>
2018-11-28 15:50:56 +0100

    Update item view

--- DesktopUI/ItemView.swift
+++ DesktopUI/ItemView.swift
@@ -3,29 +3,43 @@ import Desktop
 
 class ItemView:NSControl {
     private(set) weak var note:Note!
+    private weak var selector:NSView!
     private weak var field:NSTextField!
-    override var intrinsicContentSize:NSSize { return NSSize(width:250, height:70) }
+    override var intrinsicContentSize:NSSize { return NSSize(width:250, height:60) }
     var selected = false { didSet { update() } }
     
     init(_ note:Note) {
         self.note = note
         super.init(frame:.zero)
         translatesAutoresizingMaskIntoConstraints = false
-        wantsLayer = true
         
         let field = NSTextField()
         field.translatesAutoresizingMaskIntoConstraints = false
         field.backgroundColor = .clear
-        field.lineBreakMode = .byCharWrapping
+        field.lineBreakMode = .byWordWrapping
         field.isBezeled = false
         field.isEditable = false
+        field.font = .systemFont(ofSize:12, weight:.light)
         addSubview(field)
         self.field = field
         
-        field.topAnchor.constraint(equalTo:topAnchor, constant:10).isActive = true
-        field.leftAnchor.constraint(equalTo:leftAnchor, constant:5).isActive = true
-        field.widthAnchor.constraint(equalToConstant:240).isActive = true
-        field.heightAnchor.constraint(equalToConstant:50).isActive = true
+        let selector = NSView()
+        selector.translatesAutoresizingMaskIntoConstraints = false
+        selector.wantsLayer = true
+        selector.layer!.backgroundColor = NSColor.scotBlue.cgColor
+        addSubview(selector)
+        self.selector = selector
+        
+        field.topAnchor.constraint(equalTo:topAnchor, constant:12).isActive = true
+        field.leftAnchor.constraint(equalTo:leftAnchor, constant:20).isActive = true
+        field.widthAnchor.constraint(equalToConstant:220).isActive = true
+        field.heightAnchor.constraint(equalToConstant:36).isActive = true
+        
+        selector.topAnchor.constraint(equalTo:topAnchor).isActive = true
+        selector.bottomAnchor.constraint(equalTo:bottomAnchor).isActive = true
+        selector.rightAnchor.constraint(equalTo:rightAnchor).isActive = true
+        selector.widthAnchor.constraint(equalToConstant:2).isActive = true
+        
         update(note.content)
         update()
     }
@@ -44,14 +58,11 @@ class ItemView:NSControl {
     
     private func update() {
         if selected {
-            field.textColor = .black
-            field.font = NSFont(name:"SourceCodeRoman-Medium", size:12)!
-            layer!.backgroundColor = NSColor.scotBlue.cgColor
-            
+            field.alphaValue = 1
+            selector.isHidden = false
         } else {
-            field.textColor = .textColor
-            field.font = NSFont(name:"SourceCodeRoman-Light", size:12)!
-            layer!.backgroundColor = NSColor.clear.cgColor
+            field.alphaValue = 0.6
+            selector.isHidden = true
         }
     }
 }
