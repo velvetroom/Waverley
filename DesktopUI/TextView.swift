41e2e7a354cc9b5f3ecfb1369c03b8681e2a99db
vauxhall <zero.griffin@gmail.com>
2018-11-28 10:38:36 +0100

    Override and customise caret

--- DesktopUI/TextView.swift
+++ DesktopUI/TextView.swift
@@ -1,6 +1,9 @@
 import AppKit
 
 class TextView:NSTextView {
+    private weak var caretX:NSLayoutConstraint!
+    private weak var caretY:NSLayoutConstraint!
+    
     init() {
         super.init(frame:.zero)
         isVerticallyResizable = true
@@ -8,11 +11,29 @@ class TextView:NSTextView {
         font = NSFont(name:"SourceCodeRoman-Light", size:18)
         allowsUndo = true
         drawsBackground = false
+        
+        let caret = NSView()
+        caret.translatesAutoresizingMaskIntoConstraints = false
+        caret.wantsLayer = true
+        caret.layer!.backgroundColor = NSColor.scotBlue.cgColor
+        addSubview(caret)
+        
+        caret.widthAnchor.constraint(equalToConstant:5).isActive = true
+        caret.heightAnchor.constraint(equalToConstant:30).isActive = true
+        caretX = caret.centerXAnchor.constraint(equalTo:leftAnchor)
+        caretY = caret.centerYAnchor.constraint(equalTo:topAnchor)
+        caretX.isActive = true
+        caretY.isActive = true
     }
     
     override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
-        super.init(frame:frameRect, textContainer:container)
+        super.init(frame: frameRect, textContainer: container)
     }
     
     required init?(coder:NSCoder) { return nil }
+    
+    override func drawInsertionPoint(in rect:NSRect, color:NSColor, turnedOn flag:Bool) {
+        caretX.constant = rect.midX + 2
+        caretY.constant = rect.midY
+    }
 }
