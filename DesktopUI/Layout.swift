fc063ed5b402e357332485d7746439878a6d4069
vauxhall <zero.griffin@gmail.com>
2018-11-29 13:04:43 +0100

    Create layout manager

---
+++ DesktopUI/Layout.swift
@@ -0,0 +1,27 @@
+import AppKit
+
+class Layout:NSLayoutManager, NSLayoutManagerDelegate {
+    override init() {
+        super.init()
+        delegate = self
+    }
+    
+    required init?(coder:NSCoder) { return nil }
+    
+    func layoutManager(_:NSLayoutManager, shouldSetLineFragmentRect rect:UnsafeMutablePointer<NSRect>,
+                       lineFragmentUsedRect:UnsafeMutablePointer<NSRect>, baselineOffset
+        base:UnsafeMutablePointer<CGFloat>, in:NSTextContainer, forGlyphRange:NSRange) -> Bool {
+        base.pointee = base.pointee + ((TextView.lineHeight - rect.pointee.size.height) / 2)
+        rect.pointee.size.height = TextView.lineHeight
+        lineFragmentUsedRect.pointee.size.height = 36
+        return true
+    }
+    
+    override func setExtraLineFragmentRect(_ rect:NSRect, usedRect:NSRect, textContainer container:NSTextContainer) {
+        var rect = rect
+        var used = usedRect
+        rect.size.height = TextView.lineHeight
+        used.size.height = TextView.lineHeight
+        super.setExtraLineFragmentRect(rect, usedRect:used, textContainer:container)
+    }
+}
--- DesktopUI/TextView.swift
+++ DesktopUI/TextView.swift
@@ -1,17 +1,22 @@
 import AppKit
 
-class TextView:NSTextView, NSLayoutManagerDelegate {
+class TextView:NSTextView {
+    static let lineHeight:CGFloat = 36
     private weak var caretX:NSLayoutConstraint!
     private weak var caretY:NSLayoutConstraint!
-    
+
     init() {
-        super.init(frame:.zero)
+        let container = NSTextContainer()
+        let storage = NSTextStorage()
+        let layout = Layout()
+        storage.addLayoutManager(layout)
+        layout.addTextContainer(container)
+        super.init(frame:.zero, textContainer:container)
         isVerticallyResizable = true
         isContinuousSpellCheckingEnabled = true
         font = NSFont(name:"SourceCodeRoman-Light", size:18)
         allowsUndo = true
         drawsBackground = false
-        layoutManager!.delegate = self
         usesFindBar = true
         isIncrementalSearchingEnabled = true
         
@@ -22,24 +27,11 @@ class TextView:NSTextView, NSLayoutManagerDelegate {
         addSubview(caret)
         
         caret.widthAnchor.constraint(equalToConstant:5).isActive = true
-        caret.heightAnchor.constraint(equalToConstant:36).isActive = true
+        caret.heightAnchor.constraint(equalToConstant:TextView.lineHeight).isActive = true
         caretX = caret.centerXAnchor.constraint(equalTo:leftAnchor)
         caretY = caret.centerYAnchor.constraint(equalTo:topAnchor)
         caretX.isActive = true
         caretY.isActive = true
-    }
-    
-    func layoutManager(_:NSLayoutManager, shouldSetLineFragmentRect rect:UnsafeMutablePointer<NSRect>,
-                       lineFragmentUsedRect:UnsafeMutablePointer<NSRect>, baselineOffset
-        base:UnsafeMutablePointer<CGFloat>, in:NSTextContainer, forGlyphRange:NSRange) -> Bool {
-        base.pointee = base.pointee + ((36 - rect.pointee.size.height) / 2)
-        rect.pointee.size.height = 36
-        lineFragmentUsedRect.pointee.size.height = 36
-        return true
-    }
-    
-    override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
-        super.init(frame: frameRect, textContainer: container)
     }
     
     required init?(coder:NSCoder) { return nil }
