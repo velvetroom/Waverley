70dcc4285f7478a52c521456535a1667a4dc678d
vauxhall <zero.griffin@gmail.com>
2018-11-30 21:11:43 +0100

    Playing with string ranges

--- DesktopUI/TextView.swift
+++ DesktopUI/TextView.swift
@@ -1,6 +1,6 @@
 import AppKit
 
-class TextView:NSTextView {
+class TextView:NSTextView, NSTextStorageDelegate {
     static let lineHeight:CGFloat = 36
     private weak var caretX:NSLayoutConstraint!
     private weak var caretY:NSLayoutConstraint!
@@ -13,13 +13,14 @@ class TextView:NSTextView {
         layout.addTextContainer(container)
         super.init(frame:.zero, textContainer:container)
         isContinuousSpellCheckingEnabled = true
-        font = NSFont(name:"SourceCodeRoman-Light", size:18)
         allowsUndo = true
         drawsBackground = false
         usesFindBar = true
         isIncrementalSearchingEnabled = true
         textContainerInset.height = 50
         isRichText = false
+        font = NSFont(name:"SourceCodeRoman-Light", size:18)
+        storage.delegate = self
         
         let caret = NSView()
         caret.translatesAutoresizingMaskIntoConstraints = false
@@ -40,5 +41,31 @@ class TextView:NSTextView {
     override func drawInsertionPoint(in rect:NSRect, color:NSColor, turnedOn flag:Bool) {
         caretX.constant = rect.midX + 2
         caretY.constant = rect.midY
+    }
+    
+    func textStorage(_ storage:NSTextStorage, didProcessEditing editedMask:NSTextStorageEditActions, range editedRange:NSRange, changeInLength delta:Int) {
+//        storage.removeAttribute(.font, range:NSMakeRange(0, storage.length))
+        
+        let greeting = "Hi there! It's nice to meet you! 👋"
+        let endOfSentence = greeting.index(of: "!")!
+        let firstSentence = greeting[...endOfSentence]
+        // firstSentence == "Hi there!"
+        var string = storage.string
+//        while let range = string.range(of:"#") {
+//            NSRange(Range(string.index(before:range.upperBound), in:string), in:string)
+//            range.sub
+//            storage.addAttribute(.font, value:NSFont(name:"SourceCodeRoman-Light", size:18), range: <#T##NSRange#>)
+//            string = String(string.suffix(from:string.index(after:index)))
+//        }
+        
+        /*
+        let components = storage.string.components(separatedBy:"#")
+        var length = 0
+        for (index, string) in components.enumerated() {
+            if index > 0 {
+                NSFont(name:"SourceCodeRoman-Light", size:18)
+            }
+            length += string.count
+        }*/
     }
 }
