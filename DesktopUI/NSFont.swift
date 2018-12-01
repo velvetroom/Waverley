12cdb0020a6228212f323a14f6c6f5f0e99f46ea
vauxhall <zero.griffin@gmail.com>
2018-12-01 23:56:33 +0100

    Add font static functions

---
+++ DesktopUI/NSFont.swift
@@ -0,0 +1,6 @@
+import AppKit
+
+extension NSFont {
+    class func light(_ size:CGFloat) -> NSFont { return NSFont(name:"SourceCodeRoman-Light", size:size)! }
+    class func bold(_ size:CGFloat) -> NSFont { return NSFont(name:"SourceCodeRoman-Bold", size:size)! }
+}
