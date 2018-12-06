7b4e43e4c33a4db63af15059d20656d21755c3d4
vauxhall <zero.griffin@gmail.com>
2018-12-06 21:52:05 +0100

    Add ios icon

--- MobileUI/View.swift
+++ MobileUI/View.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 class View:UIViewController {
+    private weak var text:UITextView!
     
+    private func makeOutlets() {
+        let text = UITextView()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.backgroundColor = .clear
+        text.indicatorStyle = .white
+        text.textColor = .white
+        text.font = .systemFont(ofSize:18, weight:.light)
+        text.textContainerInset = UIEdgeInsets(top:10, left:15, bottom:20, right:15)
+        text.isEditable = false
+        view.addSubview(text)
+        self.text = text
+    }
 }
