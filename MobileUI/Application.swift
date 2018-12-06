ad983b0a065162f67df9037a5ae4b34246496a49
vauxhall <zero.griffin@gmail.com>
2018-12-06 20:43:57 +0100

    Create view for mobile

--- MobileUI/Application.swift
+++ MobileUI/Application.swift
@@ -1,11 +1,16 @@
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+@UIApplicationMain class Application:UIResponder, UIApplicationDelegate {
+    static let navigation = UINavigationController()
+    var window:UIWindow?
+    
+    func application(_:UIApplication, didFinishLaunchingWithOptions:[UIApplication.LaunchOptionsKey:Any]?) -> Bool {
+        Application.navigation.setNavigationBarHidden(true, animated:false)
+        Application.navigation.setViewControllers([View()], animated:false)
+        window = UIWindow(frame:UIScreen.main.bounds)
+        window!.backgroundColor = .black
+        window!.makeKeyAndVisible()
+        window!.rootViewController = Application.navigation
         return true
     }
 }
