a5105ba966ae743ab05921dd52110421143c5ee8
vauxhall <zero.griffin@gmail.com>
2018-12-07 09:30:52 +0100

    Create text view

---
+++ MobileUI/TextView.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class TextView:UITextView, NSTextStorageDelegate {
+    init() {
+        let container = NSTextContainer()
+        let storage = NSTextStorage()
+        let layout = NSLayoutManager()
+        storage.addLayoutManager(layout)
+        layout.addTextContainer(container)
+        super.init(frame:.zero, textContainer:container)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .clear
+        indicatorStyle = .white
+        tintColor = .scotBlue
+        alwaysBounceVertical = true
+        textColor = .white
+        returnKeyType = .default
+        keyboardAppearance = .dark
+        autocorrectionType = .yes
+        spellCheckingType = .yes
+        autocapitalizationType = .sentences
+        keyboardType = .alphabet
+        contentInset = .zero
+        textContainerInset = UIEdgeInsets(top:30, left:15, bottom:30, right:15)
+        textStorage.delegate = self
+    }
+    
+    required init?(coder:NSCoder) { return nil }
+    
+    func textStorage(_ storage:NSTextStorage, didProcessEditing:NSTextStorage.EditActions, range:NSRange,
+                     changeInLength:Int) {
+        storage.removeAttribute(.font, range:NSMakeRange(0, storage.length))
+        var lights = [NSRange]()
+        var bolds = [NSRange]()
+        var start = storage.string.startIndex
+        while let index = storage.string[start...].firstIndex(of:"#") {
+            lights.append(NSRange(start..<index, in:storage.string))
+            if let endHeading = storage.string[index...].firstIndex(of:"\n") {
+                bolds.append(NSRange(index...endHeading, in:storage.string))
+                start = endHeading
+            } else {
+                bolds.append(NSRange(index..., in:storage.string))
+                start = storage.string.endIndex
+            }
+        }
+        lights.append(NSRange(start..., in:storage.string))
+        lights.forEach { storage.addAttribute(.font, value:UIFont.editorLight(28), range:$0) }
+        bolds.forEach { storage.addAttribute(.font, value:UIFont.editorBold(28), range:$0) }
+    }
+}
--- MobileUI/View.swift
+++ MobileUI/View.swift
@@ -1,51 +1,20 @@
 import UIKit
 
-class View:UIViewController, NSTextStorageDelegate {
-    private weak var text:UITextView!
+class View:UIViewController {
+    private weak var text:TextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         makeOutlets()
     }
     
-    func textStorage(_ storage:NSTextStorage, didProcessEditing:NSTextStorage.EditActions, range:NSRange,
-                     changeInLength:Int) {
-        storage.removeAttribute(.font, range:NSMakeRange(0, storage.length))
-        var lights = [NSRange]()
-        var bolds = [NSRange]()
-        var start = storage.string.startIndex
-        while let index = storage.string[start...].firstIndex(of:"#") {
-            lights.append(NSRange(start..<index, in:storage.string))
-            if let endHeading = storage.string[index...].firstIndex(of:"\n") {
-                bolds.append(NSRange(index...endHeading, in:storage.string))
-                start = endHeading
-            } else {
-                bolds.append(NSRange(index..., in:storage.string))
-                start = storage.string.endIndex
-            }
-        }
-        lights.append(NSRange(start..., in:storage.string))
-        lights.forEach { storage.addAttribute(.font, value:UIFont.editorLight(28), range:$0) }
-        bolds.forEach { storage.addAttribute(.font, value:UIFont.editorBold(28), range:$0) }
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewDidAppear(animated)
+        text.becomeFirstResponder()
     }
     
     private func makeOutlets() {
-        let text = UITextView()
-        text.translatesAutoresizingMaskIntoConstraints = false
-        text.backgroundColor = .clear
-        text.indicatorStyle = .white
-        text.tintColor = .scotBlue
-        text.alwaysBounceVertical = true
-        text.textColor = .white
-        text.returnKeyType = .default
-        text.keyboardAppearance = .dark
-        text.autocorrectionType = .yes
-        text.spellCheckingType = .yes
-        text.autocapitalizationType = .sentences
-        text.keyboardType = .alphabet
-        text.contentInset = .zero
-        text.textContainerInset = UIEdgeInsets(top:30, left:15, bottom:30, right:15)
-        text.textStorage.delegate = self
+        let text = TextView()
         view.addSubview(text)
         self.text = text
         
